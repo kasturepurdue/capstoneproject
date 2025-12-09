3ddc9ecd58df363e47a2284266d4d7eb3d121574
Aditya Kasture <adiraaj199@gmail.com>
2025-12-09 17:32:52 -0500

    Cycle Count

---
+++ pb/pb_migrations/1765317199_updated_tests.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3643163317")
+
+  // add field
+  collection.fields.addAt(13, new Field({
+    "hidden": false,
+    "id": "number1294672336",
+    "max": null,
+    "min": null,
+    "name": "cycleCount",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3643163317")
+
+  // remove field
+  collection.fields.removeById("number1294672336")
+
+  return app.save(collection)
+})
